86a72d18372aafe602ee274ef5dde8fff4152ef4
agusquindi7 <agustinquindimiltraduccion@gmail.com>
2025-12-09 18:12:49 -0300

    Navbar responsive con hamburguesa + ajustes finales

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function Navbar({ cartCount, isAuthenticated, onLogout }) {
   const nav = useNavigate();
@@ -10,11 +11,26 @@ export default function Navbar({ cartCount, isAuthenticated, onLogout }) {
   }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
       <div className="container-fluid">
+        {/* Brand */}
         <Link className="navbar-brand" to="/">Home</Link>
 
-        <div className="collapse navbar-collapse">
+        {/* Botón hamburguesa */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Links colapsables */}
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/freelancers">Freelancers</Link>
@@ -22,16 +38,29 @@ export default function Navbar({ cartCount, isAuthenticated, onLogout }) {
             <li className="nav-item">
               <Link className="nav-link" to="/contact">Contacto</Link>
             </li>
-            
           </ul>
 
           {/* Login / Logout dinámico */}
           <ul className="navbar-nav ms-auto">
-            {!isAuthenticated && (
+            {!isAuthenticated ? (
               <li className="nav-item">
                 <Link className="nav-link" to="/login">Login</Link>
               </li>
+            ) : (
+              <li className="nav-item">
+                <button className="nav-link btn btn-link" onClick={handleLogout}>
+                  Logout
+                </button>
+              </li>
             )}
+            {/* Carrito */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/carrito">
+                🛒 {cartCount > 0 && (
+                  <span className="badge bg-light text-dark">{cartCount}</span>
+                )}
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
